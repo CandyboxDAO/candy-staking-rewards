5d3881b967b609e1729c262387829ba93127ad1c
odd-amphora <odd_amphora@protonmail.com>
2021-09-01 20:53:50 -0400

    Compiler errors

--- contracts/StakingRewards.sol
+++ contracts/StakingRewards.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 // https://docs.synthetix.io/contracts/source/interfaces/istakingrewards
 interface IStakingRewards {
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,8 @@ require("hardhat-deploy-ethers");
 
 const fs = require("fs");
 
+const defaultNetwork = "rinkeby";
+
 function mnemonic() {
   try {
     return fs.readFileSync("./mnemonic.txt").toString().trim();
@@ -12,64 +14,49 @@ function mnemonic() {
   return "";
 }
 
-function deployerPk() {
-  try {
-    return fs.readFileSync("./pk.txt").toString().trim();
-  } catch (e) {
-    console.log("Couldn't read pk", e);
-  }
-  return "";
-}
-
-const infuraId = "0e8a1922865f444683f5d0507139d739";
-
 module.exports = {
-  networks: {
-    hardhat: {
-      chainId: 1337,
-      accounts: [
+    defaultNetwork,
+
+    networks: {
+      hardhat: {
+        chainId: 1337,
+      },
+      localhost: {
+        url: "http://localhost:8545",
+      },
+      kovan: {
+        url: `https://kovan.infura.io/v3/${process.env.INFURA_ID}`,
+        accounts: {
+          mnemonic: mnemonic(),
+        },
+      },
+      rinkeby: {
+        url: `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`,
+        accounts: {
+          mnemonic: mnemonic(),
+        },
+      },
+      mainnet: {
+        url: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`,
+        accounts: {
+          mnemonic: mnemonic(),
+        },
+      },
+    },
+    solidity: {
+      compilers: [
         {
-          privateKey:
-            "c6cbd7d76bc5baca530c875663711b947efa6a86a900a9e8645ce32e5821484e",
-          balance: "1000000000000000000000",
+          version: "0.8.7",
+          settings: {
+            optimizer: {
+              enabled: true,
+              runs: 200,
+            },
+          },
         },
       ],
     },
-    localhost: {
-      url: "http://localhost:8545",
+    etherscan: {
+      apiKey: `${process.env.ETHERSCAN_API_KEY}`,
     },
-    kovan: {
-      url: "https://kovan.infura.io/v3/" + infuraId,
-      accounts: {
-        mnemonic: mnemonic(),
-      },
-    },
-    mainnet: {
-      url: "https://mainnet.infura.io/v3/" + infuraId,
-      accounts: [ deployerPk() ],
-      gasPrice: 42000000000
-    },
-  },
-  solidity: {
-    version: "0.8.6",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
-  namedAccounts: {
-    deployer: 0,
-    dev: 1,
-    fee: 2,
-  },
-  paths: {
-    sources: "./contracts",
-    cache: "./cache",
-    artifacts: "./artifacts",
-  },
-  mocha: {
-    timeout: 20000,
-  },
-};
+  };
